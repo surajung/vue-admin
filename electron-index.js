9798838da2506a10ade174801b1918158cc30773
surajung <hdoc79@gmail.com>
2017-10-18 20:02:35 +0900

    electron config

---
+++ electron-index.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow
+
+const isDev = process.env.NODE_ENV === 'development'
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  mainWindow.loadURL('file://${__dirname}/dist/index.html')
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools()
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
